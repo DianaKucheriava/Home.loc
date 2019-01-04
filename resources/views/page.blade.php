7e0ef0388d9182f39de6929d753561f3ffa8fb14
DianaKucheriava <Kucheriava.Diana@yandex.ru>
2019-01-04 15:46:12 +0200

    Update page.blade.php

--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -50,19 +50,8 @@
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                                     {{ Auth::user()->name }} <span class="caret"></span>
                                 </a>
-
                                 <ul class="dropdown-menu" role="menu">
-                                    <li>
-                                        <a href="{{ route('logout') }}"
-                                            onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                            Вихід
-                                        </a>
-
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form>
-                                    </li>
+                                    <li><a class="nav-link" href="{{ route('logout') }}">Вихід</a>
                                 </ul>
                             </li>
                         @endif
