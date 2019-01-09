13bbfd9d4e48fe31a39fb14467b07e458369b110
DianaKucheriava <Kucheriava.Diana@yandex.ru>
2019-01-09 19:14:06 +0200

    Add files via upload

---
+++ resources/views/emails/verification.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+@section('content')
+<div class="container">
+<div class="row">
+<div class="col-md-8 col-md-offset-2">
+<div class="panel panel-default">
+<div class="panel-heading">Реєстрація</div>
+<div class="panel-body">
+Для завершення реєстрації перейдіть за посиланням яке ми залишили на Вашій пошті!
+</div>
+</div>
+</div>
+</div>
+</div>
+@endsection
