3ad5021fa10930bc6c1ac6710546d6e3a089d3bf
DianaKucheriava <Kucheriava.Diana@yandex.ru>
2019-01-10 10:44:54 +0200

    Update RegisterController.php

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -2,40 +2,25 @@
 namespace App\Http\Controllers\Ajax;
 namespace App\Http\Controllers;
 namespace App\Http\Controllers\Auth;
-
 use App\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
-//use Illuminate\Http\Request;
+use App\Mail\Welcom;
 use Illuminate\Support\Facades\DB;
- use Session;
- use Request;
-
+use Illuminate\Http\Request;
+use Session;
+//use Request;
+use Illuminate\Auth\Events\Registered;
+use App\Jobs\SendVerificationEmail;
 class RegisterController extends Controller
 {
     use RegistersUsers;
-    /**
-     * Where to redirect users after registration.
-     *
-     * @var string
-     */
     protected $redirectTo = '/home';
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
     public function __construct()
     {
         $this->middleware('guest');
     }
-    /**
-     * Get a validator for an incoming registration request.
-     *
-     * @param  array  $data
-     * @return \Illuminate\Contracts\Validation\Validator
-     */
     public function showRegistrationForm()
     {
         $country = DB::table("country")->pluck("country","id_country");
@@ -59,25 +44,35 @@ class RegisterController extends Controller
             'password' => 'required|string|min:6|confirmed',
             'id_country' => 'required|integer',
             'id_region' => 'required|integer',
-            'id_city' => 'required|integer'
+            'id_city' => 'required|integer',
         ]);
     }
-    /**
-     * Create a new user instance after a valid registration.
-     *
-     * @param  array  $data
-     * @return \App\User
-     */
     protected function create(array $data)
     {
-        return User::create([
+            return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
             'id_country' => $data['id_country'],
             'id_region' => $data['id_region'],
             'id_city' => $data['id_city'],
-            'avatar'=> 'default.jpg'
+            'avatar'=> 'default.jpg',
+            'email_token' => base64_encode($data['email']),
         ]);
+    }
+    public function register(Request $request)
+    {
+        $this->validator($request->all())->validate();
+        event(new Registered($user = $this->create($request->all())));
+        dispatch(new SendVerificationEmail($user));
+        return view('emails.verification');
+    }
+    public function verify($token)
+    {
+        $user = User::where('email_token',$token)->first();
+        $user->verified = 1;
+        if($user->save()){
+            return view('emailconfirm',['user'=>$user]);
+        }
     }
 }
