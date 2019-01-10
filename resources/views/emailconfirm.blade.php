095d20e39975e6ce6aec39cdc8a0dcd7d2178119
DianaKucheriava <Kucheriava.Diana@yandex.ru>
2019-01-10 10:52:01 +0200

    Update emailconfirm.blade.php

--- resources/views/emailconfirm.blade.php
+++ resources/views/emailconfirm.blade.php
@@ -1,15 +1,15 @@
 @extends('layouts.app')
 @section('content')
 <div class="container">
-<div class="row">
-<div class="col-md-8 col-md-offset-2">
-<div class="panel panel-default">
-<div class="panel-heading">Реєстрацію завершено</div>
-<div class="panel-body">
-Ваша пошта підтверджена. Натисніть посилання <a href="{{url('/home')}}">щоб перейти на головну сторінку</a>
-</div>
-</div>
-</div>
-</div>
+  <div class="row">
+    <div class="col-md-8 col-md-offset-2">
+      <div class="panel panel-default">
+        <div class="panel-heading">Реєстрацію завершено</div>
+          <div class="panel-body">  
+            Ваша пошта підтверджена. Натисніть посилання <a href="{{url('/home')}}">щоб перейти на головну сторінку</a>
+          </div>
+        </div> 
+      </div>
+    </div>
 </div>
 @endsection
