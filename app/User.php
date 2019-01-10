d28fbb6d4aed574f1e2ecca768785754d7e40d6f
DianaKucheriava <Kucheriava.Diana@yandex.ru>
2019-01-10 10:39:43 +0200

    Update User.php

--- app/User.php
+++ app/User.php
@@ -1,23 +1,19 @@
 <?php
-
 namespace App;
-
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
     use Notifiable;
-
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password', 'id_country', 'id_region', 'id_city', 'avatar'
+        'name', 'email', 'password', 'id_country', 'id_region', 'id_city', 'avatar','email_token',
     ];
-
     /**
      * The attributes that should be hidden for arrays.
      *
