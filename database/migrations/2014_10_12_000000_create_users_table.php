77db7dccad5ff7b1d27cab663f7c99f65213c01b
DianaKucheriava <Kucheriava.Diana@yandex.ru>
2019-01-10 10:47:37 +0200

    Update 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,9 +1,7 @@
 <?php
-
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
-
 class CreateUsersTable extends Migration
 {
     /**
@@ -22,11 +20,11 @@ class CreateUsersTable extends Migration
             $table->integer('id_region');
             $table->integer('id_city');
             $table->string('avatar')->default('default.jpg');
+            $table->tinyInteger('verified')->default(0);     
             $table->rememberToken();
             $table->timestamps();
         });
     }
-
     /**
      * Reverse the migrations.
      *
