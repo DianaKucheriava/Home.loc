2f551aaa3d244f2aa5fe95fc8bc0760663c18e5d
DianaKucheriava <Kucheriava.Diana@yandex.ru>
2019-01-22 15:18:53 +0200

    Update emailconfirm.blade.php

--- resources/views/emailconfirm.blade.php
+++ resources/views/emailconfirm.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.app')
 @section('content')
+
 <div class="container">
   <div class="row">
     <div class="col-md-8 col-md-offset-2">
@@ -12,4 +13,5 @@
       </div>
     </div>
 </div>
+
 @endsection
