618c8be0bcb6d1bea44196e76dcc0bb3cbaa0598
DianaKucheriava <Kucheriava.Diana@yandex.ru>
2019-01-22 13:12:31 +0200

    Add files via upload

---
+++ resources/assets/js/app.js
@@ -0,0 +1,74 @@
+import Vue from 'vue'
+import axios from 'axios'
+import VueRouter from 'vue-router'
+import Search from './components/Search.vue'
+
+Vue.use( VueRouter );
+require('./bootstrap');
+window.Vue = require('Vue');
+Vue.component('search', require('./components/Search.vue').default);
+window.axios = require('axios');
+
+const Pos =  { template: '<h2>{{$route.params.id}}</h2>'}
+const NotFound = { template: '<h2>Страница не найдена</h2>' }
+const routes = [
+    { path: '/', component: app },
+    { path: '/search', 
+        redirect: to => {
+            if (to.params.id > 10) {
+                return '/404'
+            } else {
+                return '/post/show/:id'
+            }
+        }
+    },
+    { path: 'post/show/:id', 
+        redirect: to => {
+            if (to.params.id > 10) {
+                return '/404'
+            } else {
+                return '/post/show/:id'
+            }
+        }
+    },
+    { path: '/post/show/:id', component: Pos, name:'Post_link', props: true },
+    { path: '/404', component:NotFound },
+];
+const router = new VueRouter({
+    mode: 'history',
+    routes: routes,
+});
+const app = new Vue({
+    el: '#app',
+    router: router,
+    data: {
+        keyword: '',
+        url: 'search/searchPost', //serach
+        posts: [],// users: Array
+    },
+    mounted() {
+        this.getUser();
+    },
+    methods: {
+        getUser(url) {
+            axios.get(url).then(response => {
+                this.posts = response.data;
+            }).catch(error => {
+                console.log(Object.assign({}, error));
+            })
+        },
+        searchPost(keyword) { //searchUser(keyword)
+            this.keyword = keyword;
+            const url = `${this.url}?keyword=${this.keyword}`;
+            axios.get(url).then(response => {
+                this.posts = response.data;
+            }).catch(error => {
+                console.log(Object.assign({}, error));
+            })
+        },
+        orderRedirect: function(post) {
+            this.$router.replace('/post/show/' + post.id);
+            location.reload();
+        },
+    }
+});
