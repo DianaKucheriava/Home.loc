3147b7689ec7c5c8e8cff1dbcbd8ad5e6da15a2c
DianaKucheriava <Kucheriava.Diana@yandex.ru>
2019-01-28 17:26:48 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -31,10 +31,8 @@ Route::post('register', 'Auth\RegisterController@register')->name('post.register
 Route::post('register/{email}', array('uses' => 'EmailAvaliable@check'));
 //////////////////////////Password Reset/////////////////////////////////////////////////
 Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
-
 Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
 Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
 Route::post('password/reset', 'Auth\ResetPasswordController@reset');
 Route::get('/verifyemail/{token}', 'Auth\RegisterController@verify');
-///////////////////////
-//Route::get('search', 'PostController@search');
+
